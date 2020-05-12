3ba53a3797431b3495fe72ffdd87f4aca418a8d5
Jagadeep <54772001+jagadeep-mahendran@users.noreply.github.com>
2020-05-12 19:40:03 +0530

    Create 5. Single Element in a Sorted Array.java

---
+++ Week 2 challenge/5. Single Element in a Sorted Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNonDuplicate(int[] a) {
+        int n=a.length;
+        int res=0;
+        int f=0;
+        if(n==1)
+             return a[0];
+        for(int i=0;i<n-1;i=i+2)
+        {
+            if(a[i]!=a[i+1])
+            {
+                res=a[i];
+                f=1;
+                break;
+            }
+        }
+        if(f==0)
+             return a[n-1];
+        return  res;
+    }
+}

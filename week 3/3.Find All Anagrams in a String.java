3bf53c3b0ee075aee25b4cb0d4c17b8ea2230b65
Jagadeep <54772001+jagadeep-mahendran@users.noreply.github.com>
2020-05-21 18:34:22 +0530

    Create 3.Find All Anagrams in a String.java

---
+++ week 3/3.Find All Anagrams in a String.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        if (p == null || s == null || s.length() < p.length()) return res;
+        int m = s.length(), n = p.length();
+        for (int i = 0; i < m-n+1; i++) {
+            String cur = s.substring(i, i+n);
+            if (helper(cur, p)) res.add(i);
+        }
+        return res;
+    }
+    public boolean helper(String a, String b) {
+        if (a == null || b == null || a.length() != b.length()) return false;
+        int[] dict = new int[26];
+        for (int i = 0; i < a.length(); i++) {
+            char ch = a.charAt(i);
+            dict[ch-'a']++;
+        }
+        for (int i = 0; i < b.length(); i++) {
+            char ch = b.charAt(i);
+            dict[ch-'a']--;
+            if (dict[ch-'a'] < 0) return false;
+        }
+        return true;
+    }
+}

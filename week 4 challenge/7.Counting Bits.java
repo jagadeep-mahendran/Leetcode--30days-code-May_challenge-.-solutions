574c4f097d78a1fedcad998296c2bf098ebc5b98
Jagadeep <54772001+jagadeep-mahendran@users.noreply.github.com>
2020-05-28 17:25:21 +0530

    Update 7.Counting Bits.java

--- week 4 challenge/7.Counting Bits.java
+++ week 4 challenge/7.Counting Bits.java
@@ -1,1 +1,13 @@
 
+class Solution {
+       
+    public int[] countBits(int n) {
+          int a[]=new int[n+1]; 
+         
+        for(int i=1;i<=n;i++)
+             a[i] = a[i/2]+i%2;
+               
+        
+        return a;
+    }
+}
